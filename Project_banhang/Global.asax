be7206f39f6c28572510df23f1c1e62b570cc272
Tuấn Huy <tuanhuy030799@gmail.com>
2020-12-12 13:27:25 +0700

    Add project files.

---
+++ Project_banhang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_banhang.MvcApplication" Language="C#" %>
---
+++ Project_banhang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_banhang.Startup))]
+namespace Project_banhang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
